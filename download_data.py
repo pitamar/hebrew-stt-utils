2ed9f64fcc9fa867fdfb6abdee1174422f9df70a
Itamar Schen <itamarschen@gmail.com>
2020-03-30 17:00:29 +0300

    many improvements

--- download_data.py
+++ download_data.py
@@ -2,20 +2,22 @@ import youtube_dl
 from urllib.parse import urlparse, parse_qs
 from vtt_to_srt.__main__ import vtt_to_srt
 import os
+import yaml
 
 if __name__ == '__main__':
+    num_tries = 5
     subtitles_lang = 'iw'
 
     ydl_opts = {
         'format': 'bestaudio/best',
-        'outtmpl': 'data/%(id)s/clip.%(ext)s',
-        'postprocessors': [
-            {
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'wav',
-                'preferredquality': '0',
-            },
-        ],
+        'outtmpl': os.path.join('data', '%(id)s', 'clip.%(ext)s'),
+        # 'postprocessors': [
+        #     {
+        #         'key': 'FFmpegExtractAudio',
+        #         'preferredcodec': 'flac',
+        #         'preferredquality': '0',
+        #     },
+        # ],
         'keepvideo': True,
         'writesubtitles': True,
         'subtitleslangs': [subtitles_lang],
@@ -24,35 +26,43 @@ if __name__ == '__main__':
         'socket_timeout': 10,
     }
 
-    with open('./batch.txt') as f:
-        lines = f.read().splitlines()
-        urls = [url.strip() for url in lines if url.strip() != '' and not url.startswith('#')]
+    with open('inputs.yaml') as f:
+        inputs = yaml.safe_load(f)
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        for url in urls:
-            parsed_url = urlparse(url)
+        for input in inputs:
+            input_url = input['url']
+            parsed_url = urlparse(input_url)
             query_params = parse_qs(parsed_url.query)
 
-            if 'list' in query_params:
-                result = ydl.extract_info(url, download=False)
+            if 'v' in query_params:
+                ids = query_params['v']
+            elif 'list' in query_params:
+                result = ydl.extract_info(input_url, download=False)
 
                 ids = [entry['id'] for entry in result['entries']]
             else:
-                ids = query_params['v']
+                print('Skipping invalid youtube URL:', input_url)
+                continue
 
             for id in ids:
                 url = f'https://youtube.com/watch?v={id}'
 
-                if os.path.exists(f'data/{id}/clip.wav'):
-                    print(f'WAV file for {id} exists. Skipping')
+                if os.path.exists(os.path.join('data', id, f'clip.{subtitles_lang}.srt')):
+                    print(f'SRT file for {id} exists. Skipping')
+                elif os.path.exists(os.path.join('data', id, '.no-subtitles')):
+                    print(f'No subtitles exist for {id}. Skipping')
                 else:
                     print(f'Retrieveing clip {id} from {url}')
-                    result = ydl.extract_info(url, download=False)
+                    try:
+                        result = ydl.extract_info(url, download=False)
+                    except (youtube_dl.utils.ExtractorError, youtube_dl.utils.DownloadError) as e:
+                        print(f'Could not extract video information for {url}:', e)
+                        continue
 
                     if subtitles_lang in result['subtitles']:
                         entry_url = result['webpage_url']
 
-                        num_tries = 3
                         for i in range(num_tries):
                             try:
                                 ydl.download([entry_url])
@@ -64,10 +74,14 @@ if __name__ == '__main__':
                                 else:
                                     print(f"Couldn't download video {id} ({entry_url}).")
                     else:
-                        print(f'No subtitles exist for {id}!')
+                        os.makedirs(os.path.join('data', id), exist_ok=True)
+                        with open(os.path.join('data', id, '.no-subtitles'), 'w') as f:
+                            f.write('')
 
-                vtt_path = f'data/{id}/clip.{subtitles_lang}.vtt'
-                if os.path.exists(vtt_path):
-                    vtt_to_srt(vtt_path)
+                        print(f'Could not find subtitles for {id}!')
+
+                    vtt_path = os.path.join('data', id, f'clip.{subtitles_lang}.vtt')
+                    if os.path.exists(vtt_path):
+                        vtt_to_srt(vtt_path)
 
 print('Done')
--- prepare_manifest.py
+++ prepare_manifest.py
@@ -6,17 +6,31 @@ import os
 import shutil
 import json
 from tqdm import tqdm
+from multiprocessing import Pool
 
 language_code = 'iw'
-wav_files = glob('data/**/*.wav')
-# wav_files = glob('data/3Wa0EoXxF0o/*.wav')
-
-
+clip_format = 'webm'
 out_path = 'manifest'
 segments_path = os.path.join(out_path, 'segment_clips')
-merge_clips_threshold = 500
+merge_clips_threshold = 500  # 500 ms
+max_merge_duration = 5 * 1000   # 5 seconds
+overwrite = False
+blacklist = [
+    'כתוביות:',
+    'תכתוב:',
+    'לשידור:',
+]
+segment_padding = {
+    'start': 0,
+    'end':   0,
+}
+audio_truncate = {
+    'start': 20 * 1000,  # 10 seconds
+    'end':   20 * 1000,  # 10 seconds
+}
 
-shutil.rmtree(out_path, ignore_errors=True)
+if overwrite:
+    shutil.rmtree(out_path, ignore_errors=True)
 
 os.makedirs(out_path, exist_ok=True)
 os.makedirs(segments_path, exist_ok=True)
@@ -31,66 +45,119 @@ def filter_sub_text(str):
     return result
 
 
-manifest = []
-for wav_file in tqdm(wav_files):
-    wav_id = os.path.basename(os.path.dirname(wav_file))
-    srt_file = wav_file.replace('.wav', f'.{language_code}.srt')
+def process_clip(clip_file):
+    clip_id = os.path.basename(os.path.dirname(clip_file))
+
+    srt_file = clip_file.replace(f'.{clip_format}', f'.{language_code}.srt')
 
     if not os.path.exists(srt_file):
         print(f'Could not find {srt_file}')
-        continue
+        return []
+
+    sub_segment_dir_final = os.path.join(segments_path, clip_id)
+    if os.path.exists(sub_segment_dir_final):
+        return []
+
+    sub_segment_dir = sub_segment_dir_final + '.tmp'
+    if os.path.exists(sub_segment_dir):
+        shutil.rmtree(sub_segment_dir)
+
+    if os.path.exists(sub_segment_dir_final):
+        os.rename(sub_segment_dir_final, sub_segment_dir)
+
+    os.makedirs(sub_segment_dir, exist_ok=True)
 
     subs = pysrt.open(srt_file)
-    wav_audio = AudioSegment.from_wav(wav_file)
+    clip_audio = AudioSegment.from_file(clip_file, format=clip_format)
+    # wav_audio = AudioSegment.from_wav(clip_file)
+    clip_duration_ms = len(clip_audio)
 
     acc_sub_start_ms = None
     acc_sub_texts = []
+    clip_manifest_items = []
     for i in range(len(subs)):
         sub = subs[i]
+
+        sub_text = sub.text_without_tags
+        sub_text = re.sub('&[^&;]+;', '', sub_text)
+
+        if any(x in sub_text for x in blacklist):
+            continue
 
         sub_start_ms = sub.start.ordinal
         sub_end_ms = sub.end.ordinal
 
-        acc_sub_texts.append(sub.text_without_tags)
+        if sub_end_ms + segment_padding['end'] > clip_duration_ms - audio_truncate['end']:
+            break
+
+        if sub_start_ms - segment_padding['start'] < audio_truncate['start']:
+            continue
+
+        acc_sub_texts.append(sub_text)
         if acc_sub_start_ms is None:
             acc_sub_start_ms = sub_start_ms
 
         if i < len(subs) - 1:
             next_sub = subs[i+1]
             time_between_subs = next_sub.start.ordinal - sub.end.ordinal
-            if time_between_subs < merge_clips_threshold:
+            new_acc_duration = sub.end.ordinal - acc_sub_start_ms
+            if new_acc_duration < max_merge_duration and time_between_subs < merge_clips_threshold:
                 continue
 
-        pad_start = 0
-        pad_end = 0
-        audio_start_ms = acc_sub_start_ms - pad_start
-        audio_end_ms = sub_end_ms + pad_end
-        duration = audio_end_ms - audio_start_ms
+        audio_start_ms = acc_sub_start_ms - segment_padding['start']
+        audio_end_ms = sub_end_ms + segment_padding['end']
+        duration_in_ms = audio_end_ms - audio_start_ms
 
         sub_text = filter_sub_text(' '.join(acc_sub_texts))
-        sub_audio = wav_audio[audio_start_ms:audio_end_ms]
 
-        sub_segment_path = os.path.join(
-            segments_path,
-            wav_id,
-            f'{audio_start_ms}-{audio_end_ms}.wav',
-        )
-        os.makedirs(os.path.dirname(sub_segment_path), exist_ok=True)
-        sub_audio.export(sub_segment_path, format='wav')
+        sub_segment_path = os.path.join(sub_segment_dir, f'{audio_start_ms}-{audio_end_ms}.flac')
+
+        sub_audio = clip_audio[audio_start_ms:audio_end_ms]
+        sub_audio.export(sub_segment_path, format='flac')
 
         manifest_item = {
             'text': sub_text,
-            'audio': sub_segment_path,
-            'duration': duration,
+            'duration': duration_in_ms / 1000,
+            'audio_filepath': sub_segment_path,
             'acc_sub_texts': acc_sub_texts,
         }
 
-        manifest.append(manifest_item)
+        clip_manifest_items.append(manifest_item)
 
         acc_sub_texts = []
         acc_sub_start_ms = None
 
+    with open(os.path.join(sub_segment_dir, 'clip_manifest.json'), 'w') as f:
+        json.dump(clip_manifest_items, f, indent=True, ensure_ascii=False)
+
+    os.rename(sub_segment_dir, sub_segment_dir_final)
+    return clip_manifest_items
+
+pool = Pool(8)
+clip_files = glob(f'data/**/*.{clip_format}')
+pbar = tqdm(total=len(clip_files))
+def update(*args):
+    pbar.update()
+
+futures = []
+for clip_file in clip_files:
+    future = pool.apply_async(process_clip, [clip_file], callback=update)
+    futures.append(future)
+
+pool.close()
+pool.join()
+
+manifest = []
+for future in futures:
+    manifest.extend(future.get())
+
 with open(os.path.join(out_path, 'manifest.json'), 'w') as f:
     json.dump(manifest, f, ensure_ascii=False, indent=True)
+
+with open(os.path.join(out_path, 'jasper_manifest.json'), 'w') as f:
+    for item in manifest:
+        obj = {k: v for k, v in item.items() if k in ['text', 'duration', 'audio_filepath']}
+        json.dump(obj, f, ensure_ascii=False)
+        f.write('\n')
 
 print('Done')
