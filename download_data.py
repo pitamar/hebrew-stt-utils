a15b64d80ffc7c30584388c711db1b96dca06f49
Itamar Schen <itamarschen@gmail.com>
2020-04-02 20:51:35 +0300

    progress bars

--- download_data.py
+++ download_data.py
@@ -3,7 +3,9 @@ from urllib.parse import urlparse, parse_qs
 from vtt_to_srt.__main__ import vtt_to_srt
 import os
 import yaml
+from tqdm import tqdm
 from argparse import ArgumentParser
+from utils import suppress_stdout
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -29,64 +31,81 @@ if __name__ == '__main__':
         'extract_flat': True,
         # 'debug_printtraffic': True,
         'socket_timeout': 10,
+        'quiet': True,
     }
 
     with open('inputs.yaml') as f:
         inputs = yaml.safe_load(f)
 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    def log(obj):
+        tqdm.write(str(obj))
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl, \
+        tqdm(total=len(inputs), desc='total') as pbar, \
+        tqdm(desc='playlist') as playlist_pbar:
+
         for input in inputs:
-            input_url = input['url']
-            parsed_url = urlparse(input_url)
-            query_params = parse_qs(parsed_url.query)
+            try:
+                input_url = input['url']
+                parsed_url = urlparse(input_url)
+                query_params = parse_qs(parsed_url.query)
 
-            if 'v' in query_params:
-                ids = query_params['v']
-            elif 'list' in query_params:
-                result = ydl.extract_info(input_url, download=False)
+                if 'v' in query_params:
+                    ids = query_params['v']
+                elif 'list' in query_params:
+                    result = ydl.extract_info(input_url, download=False)
 
-                ids = [entry['id'] for entry in result['entries']]
-            else:
-                print('Skipping invalid youtube URL:', input_url)
-                continue
-
-            for id in ids:
-                url = f'https://youtube.com/watch?v={id}'
-
-                if os.path.exists(os.path.join('data', id, f'clip.{subtitles_lang}.srt')):
-                    print(f'SRT file for {id} exists. Skipping')
-                elif os.path.exists(os.path.join('data', id, '.no-subtitles')):
-                    print(f'No subtitles exist for {id}. Skipping')
+                    ids = [entry['id'] for entry in result['entries']]
                 else:
-                    print(f'Retrieveing clip {id} from {url}')
+                    log(f'Skipping invalid youtube URL: {input_url}')
+                    continue
+
+                playlist_pbar.total = len(ids)
+                playlist_pbar.n = 1
+                for id in ids:
                     try:
-                        result = ydl.extract_info(url, download=False)
-                    except (youtube_dl.utils.ExtractorError, youtube_dl.utils.DownloadError) as e:
-                        print(f'Could not extract video information for {url}:', e)
-                        continue
+                        url = f'https://youtube.com/watch?v={id}'
 
-                    if subtitles_lang in result['subtitles']:
-                        entry_url = result['webpage_url']
-
-                        for i in range(num_tries):
+                        if os.path.exists(os.path.join('data', id, f'clip.{subtitles_lang}.srt')):
+                            pass
+                            # log(f'SRT file for {id} exists. Skipping')
+                        elif os.path.exists(os.path.join('data', id, '.no-subtitles')):
+                            log(f'No subtitles exist for {id}. Skipping')
+                        else:
+                            # log(f'Retrieveing clip {id} from {url}')
                             try:
-                                ydl.download([entry_url])
-                                break
-                            except youtube_dl.utils.DownloadError as e:
-                                print('Received download error', e)
-                                if i < num_tries:
-                                    print(f'Retry #{i + 1}/{num_tries - 1}')
-                                else:
-                                    print(f"Couldn't download video {id} ({entry_url}).")
-                    else:
-                        os.makedirs(os.path.join('data', id), exist_ok=True)
-                        with open(os.path.join('data', id, '.no-subtitles'), 'w') as f:
-                            f.write('')
+                                result = ydl.extract_info(url, download=False)
+                            except (youtube_dl.utils.ExtractorError, youtube_dl.utils.DownloadError) as e:
+                                log(f'Could not extract video information for {url}:', e)
+                                continue
 
-                        print(f'Could not find subtitles for {id}!')
+                            if subtitles_lang in result['subtitles']:
+                                entry_url = result['webpage_url']
 
-                    vtt_path = os.path.join('data', id, f'clip.{subtitles_lang}.vtt')
-                    if os.path.exists(vtt_path):
-                        vtt_to_srt(vtt_path)
+                                for i in range(num_tries):
+                                    try:
+                                        ydl.download([entry_url])
+                                        break
+                                    except youtube_dl.utils.DownloadError as e:
+                                        # log(f'Received download error: {e}')
+                                        if i < num_tries:
+                                            log(f'Retry #{i + 1}/{num_tries - 1}')
+                                        else:
+                                            log(f"Couldn't download video {id} ({entry_url}).")
+                            else:
+                                os.makedirs(os.path.join('data', id), exist_ok=True)
+                                with open(os.path.join('data', id, '.no-subtitles'), 'w') as f:
+                                    f.write('')
 
-print('Done')
+                                log(f'Could not find subtitles for {id}!')
+
+                            vtt_path = os.path.join('data', id, f'clip.{subtitles_lang}.vtt')
+                            if os.path.exists(vtt_path):
+                                with suppress_stdout():
+                                    vtt_to_srt(vtt_path)
+                    finally:
+                        playlist_pbar.update()
+            finally:
+                pbar.update()
+
+log('Done')
