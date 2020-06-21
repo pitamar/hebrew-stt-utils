943c10fa7c0748fd77e185ffd147207af1327f3b
Itamar Schen <itamarschen@gmail.com>
2020-06-21 07:16:46 +0300

    print worker errors

--- prepare_manifest.py
+++ prepare_manifest.py
@@ -88,143 +88,143 @@ def filter_sub_text(str, language):
 
 
 def process_clip(clip_file, queue, language):
-    clip_id = os.path.basename(os.path.dirname(clip_file))
-    clip_format = re.sub(r'^.+\.(\w+)$', r'\1', clip_file)
+    try:
+        clip_id = os.path.basename(os.path.dirname(clip_file))
+        clip_format = re.sub(r'^.+\.(\w+)$', r'\1', clip_file)
 
-    if clip_id in clips_blacklist:
-        return []
+        if clip_id in clips_blacklist:
+            return []
 
-    srt_file = clip_file.replace(f'.{clip_format}', f'.{language_code}.srt')
+        srt_file = clip_file.replace(f'.{clip_format}', f'.{language_code}.srt')
 
-    if not os.path.exists(srt_file):
-        print(f'Could not find {srt_file}')
-        return []
+        if not os.path.exists(srt_file):
+            print(f'Could not find {srt_file}')
+            return []
 
-    sub_segment_dir_final = os.path.join(segments_path, clip_id)
-    if os.path.exists(os.path.join(sub_segment_dir_final, 'clip_manifest.json')):
-        with open(os.path.join(sub_segment_dir_final, 'clip_manifest.json')) as f:
-            clip_manifest = json.load(f)
-            return clip_manifest
+        sub_segment_dir_final = os.path.join(segments_path, clip_id)
+        if os.path.exists(os.path.join(sub_segment_dir_final, 'clip_manifest.json')):
+            with open(os.path.join(sub_segment_dir_final, 'clip_manifest.json')) as f:
+                clip_manifest = json.load(f)
+                return clip_manifest
 
-    queue.put({'action': 'set_description', 'pid': os.getpid(), 'description': clip_id})
+        queue.put({'action': 'set_description', 'pid': os.getpid(), 'description': clip_id})
 
-    sub_segment_dir = sub_segment_dir_final + '.tmp'
-    if os.path.exists(sub_segment_dir):
-        shutil.rmtree(sub_segment_dir)
+        sub_segment_dir = sub_segment_dir_final + '.tmp'
+        if os.path.exists(sub_segment_dir):
+            shutil.rmtree(sub_segment_dir)
 
-    if os.path.exists(sub_segment_dir_final):
-        os.rename(sub_segment_dir_final, sub_segment_dir)
+        if os.path.exists(sub_segment_dir_final):
+            os.rename(sub_segment_dir_final, sub_segment_dir)
 
-    os.makedirs(sub_segment_dir, exist_ok=True)
+        os.makedirs(sub_segment_dir, exist_ok=True)
 
-    subs = pysrt.open(srt_file)
-    clip_audio = AudioSegment.from_file(clip_file, format=clip_format)
-    clip_audio = clip_audio.set_channels(1)
-    clip_sample_rate = clip_audio.frame_rate
-    scale_factor = target_sample_rate / clip_sample_rate
-    clip_audio_tensor = torch.tensor([[clip_audio.get_array_of_samples()]], dtype=torch.float32, device=device)
-    clip_audio_tensor = torch.nn.functional.interpolate(clip_audio_tensor, scale_factor=scale_factor, recompute_scale_factor=False)
-    silence_points = align_subs_by_clip_silences(waveform=clip_audio_tensor, sample_rate=target_sample_rate, subs=subs)
-    # silence_subs = create_sub_for_silence_points(silence_points, target_sample_rate)
+        subs = pysrt.open(srt_file)
+        clip_audio = AudioSegment.from_file(clip_file, format=clip_format)
+        clip_audio = clip_audio.set_channels(1)
+        clip_sample_rate = clip_audio.frame_rate
+        scale_factor = target_sample_rate / clip_sample_rate
+        clip_audio_tensor = torch.tensor([[clip_audio.get_array_of_samples()]], dtype=torch.float32, device=device)
+        clip_audio_tensor = torch.nn.functional.interpolate(clip_audio_tensor, scale_factor=scale_factor, recompute_scale_factor=False)
+        silence_points = align_subs_by_clip_silences(waveform=clip_audio_tensor, sample_rate=target_sample_rate, subs=subs)
+        # silence_subs = create_sub_for_silence_points(silence_points, target_sample_rate)
 
-    # silence_srt_file = srt_file.replace('.srt', '.silence.srt')
-    # silence_subs.save(silence_srt_file)
-    #
-    # aligned_srt_file = srt_file.replace('.srt', '.aligned.srt')
-    # subs.save(aligned_srt_file)
-    #
-    # srt_to_audacity_labels(srt_file, srt_file.replace('.srt', '.txt'))
-    # srt_to_audacity_labels(aligned_srt_file, aligned_srt_file.replace('.srt', '.txt'))
-    # srt_to_audacity_labels(silence_srt_file, silence_srt_file.replace('.srt', '.txt'))
+        # silence_srt_file = srt_file.replace('.srt', '.silence.srt')
+        # silence_subs.save(silence_srt_file)
+        #
+        # aligned_srt_file = srt_file.replace('.srt', '.aligned.srt')
+        # subs.save(aligned_srt_file)
+        #
+        # srt_to_audacity_labels(srt_file, srt_file.replace('.srt', '.txt'))
+        # srt_to_audacity_labels(aligned_srt_file, aligned_srt_file.replace('.srt', '.txt'))
+        # srt_to_audacity_labels(silence_srt_file, silence_srt_file.replace('.srt', '.txt'))
 
-    clip_duration_ms = len(clip_audio)
+        clip_duration_ms = len(clip_audio)
 
-    acc_sub_start_ms = None
-    acc_sub_end_ms = None
-    acc_sub_texts = []
-    clip_manifest_items = []
-    for i in range(len(subs)):
-        sub = subs[i]
-
-        sub_text = sub.text_without_tags
-        sub_text = re.sub('&[^&;]{1,8};', '', sub_text)
-
-        if any(x in sub_text for x in language.blacklist):
-            continue
-
-        sub_start_ms = sub.start.ordinal
-        sub_end_ms = sub.end.ordinal
-
-        if sub_end_ms + segment_padding['end'] > clip_duration_ms - audio_truncate['end']:
-            break
-
-        if sub_start_ms - segment_padding['start'] < audio_truncate['start']:
-            continue
-
-        acc_sub_texts.append(sub_text)
-        filtered_sub_text = filter_sub_text(' '.join(acc_sub_texts), language)
-
-        # In case subtitle is rejected
-        if filtered_sub_text is None:
-            del acc_sub_texts[-1]
-        else:
-            acc_sub_end_ms = sub_end_ms
-
-            if acc_sub_start_ms is None:
-                acc_sub_start_ms = sub_start_ms
-
-            is_last_subtitle = i == len(subs) - 1
-            if not is_last_subtitle:
-                next_sub = subs[i+1]
-                time_between_subs = next_sub.start.ordinal - sub.end.ordinal
-                word_count = filtered_sub_text.count(' ') + 1
-                new_acc_duration = sub.end.ordinal - acc_sub_start_ms
-                if (
-                    new_acc_duration <= max_merge_duration and
-                    time_between_subs <= merge_clips_threshold and
-                    word_count <= max_word_count
-                ):
-                    continue
-
-        if acc_sub_end_ms is not None:
-            filtered_sub_text = filter_sub_text(' '.join(acc_sub_texts), language)
-
-            audio_start_ms = acc_sub_start_ms - segment_padding['start']
-            audio_end_ms = acc_sub_end_ms + segment_padding['end']
-            duration_in_ms = audio_end_ms - audio_start_ms
-
-            sub_segment_file_name = f'{audio_start_ms}-{audio_end_ms}.wav'
-
-            audio_start = audio_start_ms * (target_sample_rate // 1000)
-            audio_end = audio_end_ms * (target_sample_rate // 1000)
-
-            sub_audio_tensor = clip_audio_tensor[0, :, audio_start:audio_end] << 16
-            try:
-                audio_output_path = os.path.join(sub_segment_dir, sub_segment_file_name)
-                torchaudio.save(filepath=audio_output_path, src=sub_audio_tensor.cpu(), sample_rate=target_sample_rate, precision=16)
-            except Exception as e:
-                print(e)
-
-            manifest_item = {
-                'text': filtered_sub_text,
-                'duration': duration_in_ms / 1000,
-                'audio_filepath': os.path.join(sub_segment_dir_final, sub_segment_file_name),
-                'acc_sub_texts': acc_sub_texts,
-            }
-
-            clip_manifest_items.append(manifest_item)
-
-        acc_sub_texts = []
         acc_sub_start_ms = None
         acc_sub_end_ms = None
+        acc_sub_texts = []
+        clip_manifest_items = []
+        for i in range(len(subs)):
+            sub = subs[i]
 
-    with open(os.path.join(sub_segment_dir, 'clip_manifest.json'), 'w') as f:
-        json.dump(clip_manifest_items, f, indent=True, ensure_ascii=False)
+            sub_text = sub.text_without_tags
+            sub_text = re.sub('&[^&;]{1,8};', '', sub_text)
 
-    os.rename(sub_segment_dir, sub_segment_dir_final)
+            if any(x in sub_text for x in language.blacklist):
+                continue
 
-    queue.put({'action': 'set_description', 'pid': os.getpid(), 'description': None})
-    return clip_manifest_items
+            sub_start_ms = sub.start.ordinal
+            sub_end_ms = sub.end.ordinal
+
+            if sub_end_ms + segment_padding['end'] > clip_duration_ms - audio_truncate['end']:
+                break
+
+            if sub_start_ms - segment_padding['start'] < audio_truncate['start']:
+                continue
+
+            acc_sub_texts.append(sub_text)
+            filtered_sub_text = filter_sub_text(' '.join(acc_sub_texts), language)
+
+            # In case subtitle is rejected
+            if filtered_sub_text is None:
+                del acc_sub_texts[-1]
+            else:
+                acc_sub_end_ms = sub_end_ms
+
+                if acc_sub_start_ms is None:
+                    acc_sub_start_ms = sub_start_ms
+
+                is_last_subtitle = i == len(subs) - 1
+                if not is_last_subtitle:
+                    next_sub = subs[i+1]
+                    time_between_subs = next_sub.start.ordinal - sub.end.ordinal
+                    word_count = filtered_sub_text.count(' ') + 1
+                    new_acc_duration = sub.end.ordinal - acc_sub_start_ms
+                    if (
+                        new_acc_duration <= max_merge_duration and
+                        time_between_subs <= merge_clips_threshold and
+                        word_count <= max_word_count
+                    ):
+                        continue
+
+            if acc_sub_end_ms is not None:
+                filtered_sub_text = filter_sub_text(' '.join(acc_sub_texts), language)
+
+                audio_start_ms = acc_sub_start_ms - segment_padding['start']
+                audio_end_ms = acc_sub_end_ms + segment_padding['end']
+                duration_in_ms = audio_end_ms - audio_start_ms
+
+                sub_segment_file_name = f'{audio_start_ms}-{audio_end_ms}.wav'
+
+                audio_start = audio_start_ms * (target_sample_rate // 1000)
+                audio_end = audio_end_ms * (target_sample_rate // 1000)
+
+                sub_audio_tensor = clip_audio_tensor[0, :, audio_start:audio_end] << 16
+                audio_output_path = os.path.join(sub_segment_dir, sub_segment_file_name)
+                torchaudio.save(filepath=audio_output_path, src=sub_audio_tensor.cpu(), sample_rate=target_sample_rate, precision=16)
+
+                manifest_item = {
+                    'text': filtered_sub_text,
+                    'duration': duration_in_ms / 1000,
+                    'audio_filepath': os.path.join(sub_segment_dir_final, sub_segment_file_name),
+                    'acc_sub_texts': acc_sub_texts,
+                }
+
+                clip_manifest_items.append(manifest_item)
+
+            acc_sub_texts = []
+            acc_sub_start_ms = None
+            acc_sub_end_ms = None
+
+        with open(os.path.join(sub_segment_dir, 'clip_manifest.json'), 'w') as f:
+            json.dump(clip_manifest_items, f, indent=True, ensure_ascii=False)
+
+        os.rename(sub_segment_dir, sub_segment_dir_final)
+
+        queue.put({'action': 'set_description', 'pid': os.getpid(), 'description': None})
+        return clip_manifest_items
+    except Exception as e:
+        print(e)
 
 
 def update(*args):
